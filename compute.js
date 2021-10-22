c0c8f1ed63956c61e47f47ec7a3fe67e1a7d8256
luna <lunashoval@gmail.com>
2021-10-22 04:11:59 -0400

    compute.js

--- compute.js
+++ compute.js
@@ -7,78 +7,77 @@ console.log(Cchoice)
 }
 
 function playRound(playerSelection, computerSelection) {
-playerSelection.toLowerCase();
-let winnerC;
-let loserC;
-let lost = false;
-let won = false;
-let draw = false;
-if((computerSelection === "rock") && (playerSelection === "scissors")) {
-    winnerC = computerSelection;
-    loserC = playerSelection
-    lost = true;
-}
-if((computerSelection === "rock") && (playerSelection === "paper")) {
-    winnerC = playerSelection;
-    loserC = computerSelection
-    won = true;
-}
-if((computerSelection === "rock") && (playerSelection === "rock")) {
-    draw = true;
-}
+    playerSelection.toLowerCase();
+    let winnerC;
+    let loserC;
+    let lost = false;
+    let won = false;
+    let draw = false;
+    if((computerSelection === "rock") && (playerSelection === "scissors")) {
+        winnerC = computerSelection;
+        loserC = playerSelection
+        lost = true;
+    }
+    if((computerSelection === "rock") && (playerSelection === "paper")) {
+        winnerC = playerSelection;
+        loserC = computerSelection
+        won = true;
+    }
+    if((computerSelection === "rock") && (playerSelection === "rock")) {
+        draw = true;
+    }
 
-if((computerSelection === "scissors") && (playerSelection === "scissors")) {
-    draw = true;
-}
-if((computerSelection === "scissors") && (playerSelection === "paper")) {
-    winnerC = computerSelection;
-    loserC = playerSelection;
-    lost = true;
-}
-if((computerSelection === "scissors") && (playerSelection === "rock")) {
-    winnerC = playerSelection;
-    loserC = computerSelection;
-    won = true;
-}
+    if((computerSelection === "scissors") && (playerSelection === "scissors")) {
+        draw = true;
+    }
+    if((computerSelection === "scissors") && (playerSelection === "paper")) {
+        winnerC = computerSelection;
+        loserC = playerSelection;
+        lost = true;
+    }
+    if((computerSelection === "scissors") && (playerSelection === "rock")) {
+        winnerC = playerSelection;
+        loserC = computerSelection;
+        won = true;
+    }
 
-if((computerSelection === "paper") && (playerSelection === "scissors")) {
-    winnerC = playerSelection;
-    loserC = computerSelection;
-    won = true;
-}
-if((computerSelection === "paper") && (playerSelection === "paper")) {
-    draw = true;
-}
-if((computerSelection === "paper") && (playerSelection === "rock")) {
-    winnerC = computerSelection;
-    loserC = playerSelection;
-    lost = true;
-}
-let messageWin = "you won! " + winnerC + " beats" + loserC;
-let messageLost = "you lost! " + winnerC + " beats" + loserC;
-let messageDraw = "Its a draw!";
-if(won === true){
- return messageWin
-} else if (lost === true){
-    return messageLost
-} else if (draw === true){
-    return messageDraw
-}
-const resultView = document.quereySelector('.resultView');
-let countW = 0;
-let countL = 0;
-let countD = 0;
-if(won === true){
-    countW = countW + 1;
-}
-if(lost === true){
-    countL = countL + 1;
-}
-if(draw === true){
-    countD = countD + 1;
-}
-resultView.textContent = 'won: ' +countW +', lost: ' +countL +', draw: ' +countD;
-body.appendChild(resultView);
+    if((computerSelection === "paper") && (playerSelection === "scissors")) {
+        winnerC = playerSelection;
+        loserC = computerSelection;
+        won = true;
+    }
+    if((computerSelection === "paper") && (playerSelection === "paper")) {
+        draw = true;
+    }
+    if((computerSelection === "paper") && (playerSelection === "rock")) {
+        winnerC = computerSelection;
+        loserC = playerSelection;
+        lost = true;
+    }
+    let messageWin = "you won! " + winnerC + " beats" + loserC;
+    let messageLost = "you lost! " + winnerC + " beats" + loserC;
+    let messageDraw = "Its a draw!";
+    if(won === true){
+    return messageWin
+    } else if (lost === true){
+        return messageLost
+    } else if (draw === true){
+        return messageDraw
+    }
+    const resultView = document.quereySelector('.resultView');
+    let countW = 0;
+    let countL = 0;
+    let countD = 0;
+    if(won === true){
+        countW = countW + 1;
+    }
+    if(lost === true){
+        countL = countL + 1;
+    }
+    if(draw === true){
+        countD = countD + 1;
+    }
+    resultView.textContent = 'Results! won: ' +countW +', lost: ' +countL +', draw: ' +countD;
 }
 const resultRect = document.createElement('div');
 const btnPaper = document.querySelector('.paper');
